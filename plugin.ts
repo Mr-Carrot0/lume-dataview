c2d40f4db7470661215a4f0964e8a20072633890
Mr-Carrot0 <0mrcarrot@gmail.com>
2025-07-13 17:59:59 +0200

    Last Sync: 2025-07-13 17:59 (Mobile)

--- plugin.ts
+++ plugin.ts
@@ -1,22 +1,23 @@
 import { merge } from "lume/core/utils/object.ts";
 
 export interface Options {
-	compPath: string
-} 
+  compPath: string
+}
 
 export const defaults = {
-  compPath: "dv/"
-} 
+  compPath: "dv"
+}
 
 const components = [
   "list.vto"
-  ]
-  
+]
+
 export default function dataview(opt: Partial<Options>) {
-  const options = merge(defaults, opt )
-  
-  return (site: Site)=>{
-	  site.filter("isObj", (value)=> typeof value === "object") ;
-	  return site;
-	};
+  const options = merge(defaults, opt)
+
+  return (site: Site) => {
+    site.filter("isObj", (value:any) => typeof value === "object");
+    site.filter("isArr",(value:any)=>Array.isArray(value))
+    return site;
+  };
 } 
